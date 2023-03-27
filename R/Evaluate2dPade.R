a82eb82cb0163b185d7901f31198d5bc4fd8b3f7
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2023-03-27 08:44:31 -0400

    Finalizing 2D-Pade Poisson analysis

--- R/Evaluate2dPade.R
+++ R/Evaluate2dPade.R
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # library(dplyr)
+# source("R/load_functions_demo_biProfile.R")
 
 evaluate2dPade <- function(dataSet, folder) {
   alpha <- 0.05
@@ -21,51 +22,50 @@ evaluate2dPade <- function(dataSet, folder) {
   outcomeIds <- unique(dataSet$outcomeId)
   # i = 1
   for (i in seq_along(outcomeIds)) {
-    message(sprintf("Processing outcome %d of %d", i, length(outcomeIds)))
-    ppdata <- dataSet %>%
-      filter(.data$outcomeId == outcomeIds[i],
-             .data$analysisId == !!analysisId) %>%
-      mutate(x1 = treatment * (1 - subgroup),
-             x2 = treatment * subgroup,
-             z1 = 1,
-             z2 = subgroup) %>%
-      select(period = "time",
-             siteID = "siteId",
-             stratumID = "stratumId",
-             "x1", 
-             "x2",
-             "z1",
-             "z2",
-             "y") %>%
-      as.data.frame()
-    message("- Computing pooled estimate")
-    start <- Sys.time()
-    ResPool <- estimatePool(ppdata)
-    delta <- Sys.time() - start
-    message("  computing pooled estimate took ", signif(delta, 3), " ", attr(delta, "units"))
-    
-    message("- Computing meta-analytic estimate")
-    ResMeta <- estimateMeta(ppdata)
-    ebar <- ResMeta[[1]]
-    
-    message("- Computing local derivatives, and combining to global derivative")
-    deriv<-GetGlobalDeriv(ebar, ppdata)
-    
-    message("- Computing local derivatives")
-    J<- max(ppdata$siteID)
-    LocalDeriv <- lapply(1:J, function(j){
-      ipdata <- ppdata[ppdata$siteID==j,]
-      return(GetLocalDeriv(ebar,ipdata))
-    })
-    
-    PadeCoef <- EstPadeCoef(deriv)
-    PadeEstCR(ebar,PadeCoef)
-    
-    # table(ppdata$siteID, ppdata$y > 0)
-    
-    
-    save(ResPool, ResMeta, deriv, LocalDeriv, PadeCoef, file = file.path(folder, sprintf("PadeArtifacts_o%d.rdata", outcomeIds[i])))
-    # 
+    fileName <- file.path(folder, sprintf("PadeArtifacts_o%d.rdata", outcomeIds[i]))
+    if (!file.exists(fileName)) {
+      message(sprintf("Processing outcome %d of %d", i, length(outcomeIds)))
+      ppdata <- dataSet %>%
+        filter(.data$outcomeId == outcomeIds[i],
+               .data$analysisId == !!analysisId) %>%
+        mutate(x1 = treatment * (1 - subgroup),
+               x2 = treatment * subgroup,
+               z1 = 1,
+               z2 = subgroup) %>%
+        select(period = "time",
+               siteID = "siteId",
+               stratumID = "stratumId",
+               "x1", 
+               "x2",
+               "z1",
+               "z2",
+               "y") %>%
+        as.data.frame()
+      message("- Computing pooled estimate")
+      start <- Sys.time()
+      ResPool <- estimatePool(ppdata)
+      delta <- Sys.time() - start
+      message("  computing pooled estimate took ", signif(delta, 3), " ", attr(delta, "units"))
+      
+      message("- Computing meta-analytic estimate")
+      ResMeta <- estimateMeta(ppdata)
+      ebar <- ResMeta[[1]]
+      
+      message("- Computing local derivatives, and combining to global derivative")
+      deriv<-GetGlobalDeriv(ebar, ppdata)
+      
+      message("- Computing local derivatives")
+      J<- max(ppdata$siteID)
+      LocalDeriv <- lapply(1:J, function(j){
+        ipdata <- ppdata[ppdata$siteID==j,]
+        return(GetLocalDeriv(ebar,ipdata))
+      })
+      
+      PadeCoef <- EstPadeCoef(deriv)
+      PECR <- PadeEstCR(ebar,PadeCoef)
+      save(ResPool, ResMeta, deriv, LocalDeriv, PadeCoef, PECR, file = fileName)
+      
+    } 
     # e <- new.env()
     # load(file = file.path(folder, sprintf("PadeArtifacts_o%d.rdata", outcomeIds[i])), env = e)
     # ls(envir = e)
